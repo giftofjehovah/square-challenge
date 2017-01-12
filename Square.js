4cd1b675692944a10229c2e7591666a1ce3ea914
Jonathan Phoon <jonathan.pwh@gmail.com>
2017-01-12 23:09:51 +0800

    refactor render method

--- Square.js
+++ Square.js
@@ -5,12 +5,19 @@ class Square {
   generate (char, times) {
     return Array(times).fill(char).join('')
   }
+  ifIsFirstAndLast (index) {
+    return index === 0 || index === this.length - 1
+  }
+  generateFirstOrLastRow () {
+    return this.generate('#', this.length)
+  }
+  generateMidRows () {
+    return this.generate('#', 1) + this.generate(' ', this.length - 2) + this.generate('#', 1)
+  }
   render () {
     let row = ''
     for (var i = 0; i < this.length; i++) {
-      row = i === 0 || i === this.length - 1
-      ? this.generate('#', this.length)
-      : this.generate('#', 1) + this.generate(' ', this.length - 2) + this.generate('#', 1)
+      row = this.ifIsFirstAndLast(i) ? this.generateFirstOrLastRow() : this.generateMidRows()
       console.log(row)
     }
   }
