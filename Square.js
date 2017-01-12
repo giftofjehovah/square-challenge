33a501bd725434e4ffd721150e756eca6172f5ee
Jonathan Phoon <jonathan.pwh@gmail.com>
2017-01-12 22:23:37 +0800

    done

--- Square.js
+++ Square.js
@@ -1,7 +1,19 @@
-class Sqaure {
+class Square {
   constructor (length) {
     this.length = length
   }
+  generate (char, times) {
+    return Array(times).fill(char).join('')
+  }
+  render () {
+    let row = ''
+    for (var i = 0; i < this.length; i++) {
+      row = i === 0 || i === this.length - 1
+      ? this.generate('#', this.length)
+      : this.generate('#', 1) + this.generate(' ', this.length - 2) + this.generate('#', 1)
+      console.log(row)
+    }
+  }
 }
 
-module.exports = Sqaure
+module.exports = Square
--- __tests__/square-test.js
+++ __tests__/square-test.js
@@ -1,6 +1,24 @@
-/* global test expect */
+/* global test expect describe */
 const Square = require('../Square')
-test('square length to be same as constructor arg', () => {
+
+describe('Sqaure Class', () => {
   const square = new Square(4)
-  expect(square.length).toBe(4)
+
+  test('square length to be same as constructor arg', () => {
+    expect(square.length).toBe(4)
+  })
+
+  test('generate 3 space', () => {
+    const threeSpace = square.generate(' ', 3)
+    expect(threeSpace).toBe('   ')
+  })
+
+  test('generate 4 hex', () => {
+    const threeSpace = square.generate('#', 4)
+    expect(threeSpace).toBe('####')
+  })
+
+  test('render square of length 4', () => {
+    square.render()
+  })
 })
